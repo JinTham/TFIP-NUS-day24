133b2188b54f75492fff3e5637aecb783fc34978
Yeh Jin Tham <yehjintham@gmail.com>
2023-03-14 12:08:17 +0800

    commit

--- src/main/java/tfip/paf/day24/Controllers/EmployeeController.java
+++ src/main/java/tfip/paf/day24/Controllers/EmployeeController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import tfip.paf.day24.Models.Employee;
@@ -18,10 +22,50 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeSvc;
 
+    //List all employees
     @GetMapping
     public String listEmployees(Model model) {
         List<Employee> employees = employeeSvc.findAll();
         model.addAttribute("employees", employees);
         return "employeeList";
-    } 
+    }
+
+    //Create new employee
+    @GetMapping(path="/addnew")
+    public String createEmployee(Model model) {
+        Employee newEmployee = new Employee();
+        model.addAttribute("newEmployee", newEmployee);
+        return "createEmployee";
+    }
+    @PostMapping(path="/save")
+    public String addNewEmployee(@ModelAttribute("newEmployee") Employee newEmployee, BindingResult result) {
+        if (result.hasErrors()) {
+            return "createEmployee";
+        }
+        employeeSvc.save(newEmployee);
+        return "redirect:/employees";
+    }
+
+    //Delete employee
+    @GetMapping(path="/delete/{employee-id}")
+    public String deleteEmployee(@PathVariable("employee-id") Integer id) {
+        employeeSvc.delete(id);
+        return "redirect:/employees";
+    }
+
+    //Edit employee
+    @GetMapping(path="/edit/{employee-id}")
+    public String updateEmployee(@PathVariable("employee-id") Integer id, Model model) {
+        Employee retrievedEmployee = employeeSvc.findById(id);
+        model.addAttribute("employee", retrievedEmployee);
+        return "updateEmployee";
+    }
+    @PostMapping(path="/saveUpdate")
+    public String updateEmployee(@ModelAttribute("employee") Employee employee, BindingResult result) {
+        if (result.hasErrors()) {
+            return "updateEmployee";
+        }
+        employeeSvc.save(employee);
+        return "redirect:/employees";
+    }
 }
